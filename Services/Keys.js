4fcbe37c382e31d4aec9c42296a18fae666336ef
Liam Bryant <liambryant@outlook.it>
2019-09-17 10:00:21 +0200

    Minor Correction to keys.js

--- Services/Keys.js
+++ Services/Keys.js
@@ -7,7 +7,8 @@ module.exports = {
         TWITTER_CONSUMER_KEY : "Insert Your data",
         TWITTER_CONSUMER_SECRET : "Insert Your data",
         ACCESS_TOKEN : "Insert Your data",
-        ACCESS_TOKEN_SECRET : "Insert Your data"
+        ACCESS_TOKEN_SECRET : "Insert Your data",
+        CB_URL:"Insert Callback URL"
 
     },
 
